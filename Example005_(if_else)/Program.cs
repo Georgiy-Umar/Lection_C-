e24568a5777ec414cd887040c06e4d88e7dec19e
Umar <abnazyrov.george@gmail.com>
2022-10-02 22:43:00 +0200

    Add if/else construction

---
+++ Example005_(if_else)/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "umar")
+{
+Console.WriteLine("AssalamuAleikum Umar");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
