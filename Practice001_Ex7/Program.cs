4dea278eaf76d3eef56cefaef4cb042732b0ad66
Umar <abnazyrov.george@gmail.com>
2022-10-04 16:17:58 +0200

    Add example 7 in the practice

---
+++ Practice001_Ex7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter any three-digit numer N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n < 100 | n > 999)
+{
+Console.Write("Check your input, you did enter incorrect number and repeat your enter: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("The last digit of the number: " + Convert.ToString(n % 10));
