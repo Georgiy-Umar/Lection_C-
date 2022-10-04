82d6f0a5885d3aa8967d07192df112070e03eef3
Umar <abnazyrov.george@gmail.com>
2022-10-04 07:37:17 +0200

    Add new folders from prictice_001 + Ex_2

---
+++ Practice001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter the first entereger number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second entereger number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (n == a * a)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.Write("No");
+    Console.WriteLine();
+}
---
+++ Practice001_Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a number of the weeks day (from 1 to 7): ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+while (weekDay < 1| weekDay > 7) 
+{
+    Console.WriteLine("You did Enter the incorrect number, repeat your enter!: ");
+    weekDay = Convert.ToInt32(Console.ReadLine());
+}
+if (weekDay == 1) Console.WriteLine("Monday");
+else if (weekDay == 2) Console.WriteLine("Tuesday");
+else if (weekDay == 3) Console.WriteLine("Thirsday");
+else if (weekDay == 4) Console.WriteLine("Wednesday");
+else if (weekDay == 5) Console.WriteLine("Friday");
+else if (weekDay == 6) Console.WriteLine("Saturday");
+else if (weekDay == 7) Console.WriteLine("Sunday");
+
+    Console.WriteLine("Great! Thank you Umar");
