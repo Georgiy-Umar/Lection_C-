162e9ced09ef7e0d842e82b10db1c46cb9f31873
Umar <abnazyrov.george@gmail.com>
2022-10-06 22:48:12 +0200

    Edited name from Practice001_Ex09 to Practice002_Ex09

---
+++ Practice002_Ex09/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которую выводит случайное число из отрезка [10,99]
+ и показывает наибольшую цифру числа.*/
+
+ int n = new Random().Next(10, 99);
+// команда new добавляет новый объект рандома//
+ Console.WriteLine(n);
+//Данной конструкцией мы определили рандомно переменную N//
+int first = n/10;
+int second = n%10;
+
+Console.WriteLine(first);
+Console.WriteLine(second);
+
+if(first > second)
+{
+    Console.WriteLine(first);
+}
+else
+{
+    Console.WriteLine(second);
+}    
