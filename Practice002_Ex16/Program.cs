d9e207466ef6afd688a1a197e65d1158915df021
Umar <abnazyrov.george@gmail.com>
2022-10-07 07:58:17 +0200

    added Practice002_ex16

---
+++ Practice002_Ex16/Program.cs
@@ -0,0 +1,16 @@
+﻿ /*Необходимо написать программу, которая принимает на вход два числа
+ и проверяет, является ли одно число квадратом другого */
+
+ Console.Write("Enter any number a: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter any number b: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b || a / b == b)
+{
+    Console.WriteLine("The number a is square of number b");
+}
+else
+{
+Console.WriteLine("The number is't square");
+}
--- Practice002_Ex16/obj/Debug/net6.0/Practice002_Ex16.AssemblyInfo.cs
+++ Practice002_Ex16/obj/Debug/net6.0/Practice002_Ex16.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Practice001_Ex09")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Practice002_Ex16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Practice001_Ex09")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Practice001_Ex09")]
+[assembly: System.Reflection.AssemblyProductAttribute("Practice002_Ex16")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Practice002_Ex16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
