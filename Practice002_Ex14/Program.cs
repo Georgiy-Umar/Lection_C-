44fb0a8628510fc6754f6a28a58a00475f407d82
Umar <abnazyrov.george@gmail.com>
2022-10-07 07:38:58 +0200

    added Practice002Ex14

---
+++ Practice002_Ex14/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Написать программу которая принимает
+ на вход число и проверяет кратно ли оно одновременное 7 и 23. */
+
+Console.WriteLine("Enter any number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+Console.WriteLine("Excelent! Your number is a multiple 7 and 23");
+}
+else
+{
+Console.WriteLine("Excelent! Your number is not a multiple 7 and 23");  
+}
