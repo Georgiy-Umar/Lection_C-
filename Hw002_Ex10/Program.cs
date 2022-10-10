cbf400232cd59f73b76d95185cef0b54226b91cc
Umar <abnazyrov.george@gmail.com>
2022-10-10 13:23:41 +0200

    Lection003_aboutMethods, 4 methods in SVC

---
+++ Hw002_Ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+int n = 0;
+Console.WriteLine("Enter your any number from 100 to 999: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+if (n < 100 | n > 999)
+{
+    Console.WriteLine("Error!You didn't enter number from 100 to 999, try again: ");
+}
+else
+{
+    Console.WriteLine();
+}
+
+
+
+/*if (n > 100 && n < 999)
+{
+    Console.WriteLine(+""+ second +""+);
+*/
---
+++ Lection003_aboutMethods/Program.cs
@@ -0,0 +1,60 @@
+﻿/*/ Вид методов №1
+void Method1()
+{
+    Console.WriteLine("Автор Аб"); //что-то вывести через команду,где в конце программы хочу указать свое авторство
+}
+/* Вариант как вызываются функции:
+1 необходимо указать идентификатор этого метода как в примере выше: "Method1();"
+Method1(); */
+
+/* Метод№2:
+Ничего не возвращают, но могут принимать какие-то аргументы:
+
+void Method2(string msg)
+{
+Console.WriteLine(msg);
+}
+Method2("Text message"); /*также бывают именованные аргументы, 
+где хотим указать к какому аргументу какое значение хотим указать 
+Например: Method2(msg: text of massege); - это нужно когда методы 
+принимают какое-то несколько аргументов. */
+
+/*void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;  
+        ИНКРИМЕНТ - увеличение счетчика на 1 ед.
+        ДЕКРИМЕНТ - уменьшение счетчика на 1 ед.
+    }
+}
+Method21(count: 4, msg: "New Text 4 raza" );
+** также можно не попорядку: Method21(count: 4, msg: "Text 4 raza"); */
+
+/* Метод№3
+Должны в обязательном порядке указать тип данных, которые мы ожидаем. 
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int Year = Method3();
+Console.WriteLine(Year); */
+
+/* Метод№4
+*/
+string Method4(int count, string document)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while (i < count)
+    {
+        result = result + document;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "text budem skleivat 10 raz");
+Console.WriteLine(res);
