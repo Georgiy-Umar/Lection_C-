77c3bae44b5cbdc4e37527a59566175921a378fb
Umar <abnazyrov.george@gmail.com>
2022-10-12 13:44:26 +0200

    Practice003_Ex22

---
+++ Practice003_Ex22/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на входе
+// число N и выдает таблицу квадратов чисел от 1 до N
+// 5 -> 1,4,9,16,25
+// 2 -> 1,2
+Console.Write("Enter any numbers: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N;i++)
+{
+    Console.Write($"{i * i} ");
+}
+
+
+
+
+
