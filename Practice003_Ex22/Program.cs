ce2cf38e8e2e7ef1611064ef070a7b32bc6871de
Umar <abnazyrov.george@gmail.com>
2022-10-13 13:59:02 +0200

    Ex22 adited new variant

--- Practice003_Ex22/Program.cs
+++ Practice003_Ex22/Program.cs
@@ -2,6 +2,31 @@
 // число N и выдает таблицу квадратов чисел от 1 до N
 // 5 -> 1,4,9,16,25
 // 2 -> 1,2
+/*bool one = true;  //этот тоже отличный вариант кода!!!
+while (one)
+{
+    try 
+    {
+        Console.WriteLine("Enter any number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int index = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            index = (int)Math.Pow(i,2); // возведение в степень числа и в 2ю степень
+            if (i == num) Console.Write(index + ".");
+            else
+            Console.Write(index + ",");
+        } 
+        one = false;
+    }
+    catch
+    {
+        Console.WriteLine("Error! Try again: ");
+    }
+
+}
+*/
+
 Console.Write("Enter any numbers: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
