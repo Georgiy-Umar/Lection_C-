8f470cf159823de038ae566da896af5c27020b82
Umar <abnazyrov.george@gmail.com>
2022-10-17 23:07:38 +0200

    Practice005_Ex32

---
+++ Practice005_Ex32/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] array = new Int32[4];
+
+randomFilling(array, min:-9, max:10); 
+arrayOutput(array);
+singChange(array);
+arrayOutput(array);
+
+void singChange(int[] arr) // функция заготовка
+{
+for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1; //метода преобразования поменять знак у элемента массива
+    }
+
+}
+
+
+void randomFilling(int[] arr, int min, int max) // функция заготовка
+{
+for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max);
+    }
+
+}
+void arrayOutput(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+       { 
+        Console.Write($"{arr[i]}");
+        if(i < (arr.Length-1)) Console.Write($", ");
+       }
+       Console.WriteLine();
+}
