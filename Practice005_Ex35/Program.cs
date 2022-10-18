f16bdd8974ad5020896637d424d082bc27e40f10
Umar <abnazyrov.george@gmail.com>
2022-10-18 23:49:33 +0200

    Practice005_Ex33-35

---
+++ Practice005_Ex35/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задайте одномерный массив из 123 случайных чисел. 
+Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5 */
+int[] arr = new Int32[10];
+fillMass(arr, -10, 120);
+printArray(arr);
+numFind(arr);
+
+int[] fillMass(int[] array, int from, int to)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(from, to +1);
+    }
+    return array;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void numFind(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+            if (array[i] >= 10 && array[i] <= 99)
+            {
+                sum ++;
+            }
+    }
+        Console.WriteLine(sum);
+}
